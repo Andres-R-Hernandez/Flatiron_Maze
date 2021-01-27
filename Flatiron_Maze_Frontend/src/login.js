05447c4c0f400102ccf655fa95d1ff92156ecd86
Andres-R-Hernandez <andres.hernandez@utexas.edu>
2021-01-27 15:47:05 -0600

    create a player function

--- Flatiron_Maze_Frontend/src/login.js
+++ Flatiron_Maze_Frontend/src/login.js
@@ -22,6 +22,18 @@ function renderLogin() {
         event.preventDefault()
         login(event.target.userName.value)
     })
+    
+    //add create a player button
+    let newPlayerBtn = document.createElement('button')
+    newPlayerBtn.innerText = "Create A PLayer"
+    newPlayerBtn.classList.add("btn")
+    newPlayerBtn.classList.add("btn-primary")
+    newPlayerBtn.classList.add("mt-2")
+    newPlayerBtn.addEventListener('click', () => {
+        console.log("clicked")
+        renderCreatePlayerForm()
+    })
+    loginContainer.append(newPlayerBtn)
 }
 
 function login(userName) {
@@ -42,6 +54,26 @@ function login(userName) {
             console.log(error.message);
         });
     }
+}
+
+function renderCreatePlayerForm() {
+    let loginContainer = document.querySelector("#login-form-container");
+    loginContainer.innerHTML = ""
+    loginContainer.innerHTML = 
+    `<form id="login-form" class="m-3">
+        <p class="text-light">Create Your Player!</p>
+        <div class="row">
+            <div class="col-sm">
+                <input id="namevalue" name="userName" type="text" class="form-control" placeholder="Enter a Name">
+            </div>
+        </div>
+        <input class="btn btn-primary mt-2" id="submit" type="submit">
+    </form>`;
+    let loginForm = document.querySelector("#login-form");
+    loginForm.addEventListener('submit', (event) => {
+        event.preventDefault()
+        createNewPlayer(event.target.userName.value)
+    })
 }
 
 function createNewPlayer(name) {
